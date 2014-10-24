34bcd574d91553b388ea0dd30d2ee6621691f7cc
jfinucane <jimway@gmail.com>
2014-10-24 10:41:48 -0400

    railscast on 4.0 upgrade

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
- require "action_controller/railtie"
- require "action_mailer/railtie"
- #require "active_resource/railtie"
- require "rails/test_unit/railtie"
- require "sprockets/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+#require "active_resource/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  #Bundler.require(:default, :assets, Rails.env)
-end
+Bundler.require(:default, Rails.env)
 
 module Galaxy
   class Application < Rails::Application
@@ -58,7 +53,8 @@ module Galaxy
     #config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    # asset pipepline removed per Railscast 415
+    # config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
