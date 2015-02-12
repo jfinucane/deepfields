6484b83936140e1c772caf0aa1e88eb3784f7f07
jfinucane <jimway@gmail.com>
2015-02-12 18:27:50 -0500

    use ng-if to control video player- hiding doesnt prevent sound

--- app/assets/javascripts/angular/services/modal_service.js
+++ app/assets/javascripts/angular/services/modal_service.js
@@ -1,38 +1,22 @@
 game.service('modalService', function() {
 
-	var modal_stack = []
-
-
-    this.modal = {}
-
-	this.open = function(id) {
-		modal_stack.push(id)
-		video_available()
-		console.log('stacked',id)
-	} 
+  var modal_stack = []
+  this.open = function(id) {
+      modal_stack.push(id)
+      console.log('stacked',id)
+  }
 // all modals stack on top of each other, so remove them in LIFO order
-	this.close = function() {
+  this.close = function() {
        id = modal_stack.pop()
-       video_available()
        console.log('trying to close',id)
 	}
-    
-    this.show =function(id) {
-    	return ( modal_stack.indexOf(id) > -1)
-    }
-    var video_available = function () {
-        var video_pos= modal_stack.indexOf('Meet the HDFs')
+  this.show =function(id) {
+      return ( modal_stack.indexOf(id) > -1)
+  }
+// TODO hardwired to the name of the modal with the video.
+  this.video_on_top = function () {
+        var video_pos= modal_stack.indexOf('Meet the Hubble Deep Fields')
         var video_last =  (video_pos == modal_stack.length-1) &&  (video_pos > -1)
-        if (video_last){ 
-          if ($('.inner_frame_video').length==0) { $('.outer_frame_video').append(iframe[0].outerHTML); console.log('attached', iframe[0].outerHTML)}
-          }
-        else {
-          if ($('.inner_frame_video').length>0) {iframe = $('.inner_frame_video').detach(); console.log('detached', iframe[0].outerHTML)}
-        }
-    }
-
-    this.modal.open = this.open
-    this.modal.close = this.close
-    this.modal.show = this.show
-    this.modal.video_available = this.video_available
+        return video_last
+  }
 });
