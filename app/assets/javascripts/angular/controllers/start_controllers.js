f0a4a93ea93536612fe43b67f80dfeda9715bb52
jfinucane <jimway@gmail.com>
2015-02-24 13:24:42 -0500

    start/irregulars and start/next

--- app/assets/javascripts/angular/controllers/start_controllers.js
+++ app/assets/javascripts/angular/controllers/start_controllers.js
@@ -1,16 +1,12 @@
 'use strict';
-
-
 gameControllers.controller('outbackCtrl', ['$scope', '$routeParams', '$injector', 'pageStatus',
   function($scope, $routeParams, $injector,page) {
     page($scope,3)
   }]);
-
 gameControllers.controller('stareCtrl', ['$scope', '$routeParams', '$injector', 'pageStatus',
   function($scope, $routeParams, $injector,page) {
     page($scope,4)
   }]);
-
 gameControllers.controller('classicCtrl', ['$scope', '$routeParams', '$injector', 'pageStatus',
   function($scope, $routeParams, $injector,page) {
     page($scope,5)
@@ -21,25 +17,25 @@ gameControllers.controller('selectCtrl', ['$scope', '$routeParams', '$injector',
     page($scope,6)
     galaxyData.reset_the_counts()
     $scope.galaxyData = galaxyData
+    $scope.clicksAreActive = true
     $scope.got_counts = function(){
-      galaxyData.compute_frequencies()
+      galaxyData.compute_frequencies()  //Memoize frequencies to avoid $digest loop
       location.path('/start/got_counts')
     }
   }]);
-
 gameControllers.controller('gotCountsCtrl', ['$scope', '$routeParams', '$injector', 'pageStatus','galaxyData',
   function($scope, $routeParams, $injector,page, galaxyData) {
     page($scope,7)
-    galaxyData.redraw_galaxies
+    $scope.clicksAreActive = false
+    galaxyData.redraw_galaxies()
     $scope.galaxyData = galaxyData
-
   }]);
-
-gameControllers.controller('IrregularsCtrl', ['$scope', '$routeParams', '$injector', 'pageStatus',
-  function($scope, $routeParams, $injector,page) {
+gameControllers.controller('irregularCtrl', ['$scope', '$routeParams', '$injector', 'pageStatus', 'galaxyData',
+  function($scope, $routeParams, $injector,page, galaxyData) {
+    $scope.galaxyData = galaxyData
     page($scope,8)
+    $scope.show_me_result=false;
   }]);
-
 gameControllers.controller('nextCtrl', ['$scope', '$routeParams', '$injector', 'pageStatus',
   function($scope, $routeParams, $injector,page) {
     page($scope,9)
--- app/assets/javascripts/finder.js.coffee
+++ app/assets/javascripts/finder.js.coffee
@@ -1,19 +1,4 @@
-jQuery ->
-  if window.page('Got Counts')
-    galaxies = JSON.parse($('#galaxies').html().trim())
-    $.ajax({url: json_data()}).done \
-      (locations)->
-        drawX locations[galaxy_id] for galaxy_id in galaxies  
-jQuery ->
-  if window.page('Irregulars')
-    $('#show_me').click () ->
-      $('.guess_yes_no').removeClass('hide')
-      $('#show_me').addClass('hide')
-      $('#astronomers_result').removeClass('hide')
-      $('.click_to_show').addClass('hide')
-
-      $('.next_disable_signpost').addClass('hide')
-      $('.next_signpost').removeClass('hide')
+ 
 $('#why_link').click () ->
   $('.left_of_galaxy_windowshade').animate
     height: '330px',\
